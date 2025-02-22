8000d7dec13b94b0544395959c8b344a8c7f6123
kelvin <kelkipro@gmail.com>
2025-02-22 10:25:41 +0300

    data-structures

--- lib/data_structures.py
+++ lib/data_structures.py
@@ -18,6 +18,7 @@ spicy_foods = [
 
 def get_names(spicy_foods):
     return [food["name"] for food in spicy_foods]
+print(get_names(spicy_foods))
 def get_spiciest_foods(spicy_foods):
     return [food for food in spicy_foods if food["heat_level"]>5]
 
@@ -42,3 +43,5 @@ def get_average_heat_level(spicy_foods):
 def create_spicy_food(spicy_foods, spicy_food):
     spicy_foods.append(spicy_food)
     return spicy_foods
+
+
